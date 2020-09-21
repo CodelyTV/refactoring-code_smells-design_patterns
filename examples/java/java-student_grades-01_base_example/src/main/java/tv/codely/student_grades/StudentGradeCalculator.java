042fe48ea6cc4b5901fd2902b18dce650e87aafc
Javier Ferrer <javier.mailserio@gmail.com>
2020-09-21 17:02:12 +0200

    Implement basic StudentGradeCalculator

--- examples/java/java-student_grades-01_base_example/src/main/java/tv/codely/student_grades/StudentGradeCalculator.java
+++ examples/java/java-student_grades-01_base_example/src/main/java/tv/codely/student_grades/StudentGradeCalculator.java
@@ -3,11 +3,19 @@ package tv.codely.student_grades;
 import java.util.List;
 
 public class StudentGradeCalculator {
-    public Integer calculateGrades(List<Integer> examsGrades) {
+    public Float calculateGrades(final List<Float> examsGrades) {
         if (!examsGrades.isEmpty()) {
-            return 1;
+            Float gradesSum = 0.f;
+            Integer gradesCounter = 0;
+
+            for (Float examGrade : examsGrades) {
+                gradesSum += examGrade;
+                gradesCounter++;
+            }
+
+            return gradesSum / gradesCounter;
         } else {
-            return 0;
+            return 0.f;
         }
     }
 }
--- examples/java/java-student_grades-01_base_example/src/test/java/tv/codely/student_grades/StudentGradeCalculatorShould.java
+++ examples/java/java-student_grades-01_base_example/src/test/java/tv/codely/student_grades/StudentGradeCalculatorShould.java
@@ -5,13 +5,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 public class StudentGradeCalculatorShould {
 
 	@Test
-	void return_0_with_no_exam_grades() {
+	void calculate_0_with_no_exam_grades() {
 		StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
 
-		assertEquals(0, studentGradeCalculator.calculateGrades(Collections.emptyList()));
+        final List<Float> examsGrades = Collections.emptyList();
+
+        assertEquals(0, studentGradeCalculator.calculateGrades(examsGrades));
+	}
+
+	@Test
+	void calculate_same_grade_with_one_single_exam() {
+		StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+
+        final List<Float> examsGrades = List.of(5.f);
+
+        assertEquals(5, studentGradeCalculator.calculateGrades(examsGrades));
+	}
+
+	@Test
+	void calculate_average_grade_based_on_different_exam_grades() {
+		StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+
+        final List<Float> examsGrades = List.of(5.f, 4.f, 6.f, 2.f, 8.f, 0.f, 10.f);
+
+        assertEquals(5, studentGradeCalculator.calculateGrades(examsGrades));
+	}
+
+	@Test
+	void round_up_to_2_decimals() {
+		StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+
+        final List<Float> examsGrades = List.of(5.f, 4.f);
+
+        assertEquals(4.5f, studentGradeCalculator.calculateGrades(examsGrades));
 	}
 }
