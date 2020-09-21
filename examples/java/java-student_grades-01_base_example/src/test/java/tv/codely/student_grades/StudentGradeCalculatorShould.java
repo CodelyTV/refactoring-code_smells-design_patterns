51bb022dcd571ff9cd6e02b3af727f3e0644fff4
Javier Ferrer <javier.mailserio@gmail.com>
2020-09-21 17:10:46 +0200

    Benevolent teachers can decide if we increase one extra point to all studens (code logic in order to be extracted in its own method and illustrate complex if conditions)

--- examples/java/java-student_grades-01_base_example/src/test/java/tv/codely/student_grades/StudentGradeCalculatorShould.java
+++ examples/java/java-student_grades-01_base_example/src/test/java/tv/codely/student_grades/StudentGradeCalculatorShould.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StudentGradeCalculatorShould {
     @Test
     void fail_given_there_are_no_exams() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades              = Collections.emptyList();
         final boolean                    hasReachedMinimumClasses = true;
@@ -20,7 +20,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void calculate_same_grade_given_one_single_exam_and_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 5f));
         final boolean                    hasReachedMinimumClasses = true;
@@ -30,7 +30,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void calculate_average_grade_given_different_exam_grades_and_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades = List.of(
             new Pair<>(10, 4f),
@@ -51,7 +51,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void round_up_to_2_decimals_given_odd_exam_grades_and_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades = List.of(
             new Pair<>(50, 4f),
@@ -66,7 +66,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void fail_when_there_are_no_exams_and_has_not_attended_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades              = Collections.emptyList();
         final boolean                    hasReachedMinimumClasses = false;
@@ -76,7 +76,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void fail_given_one_single_exam_but_not_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 5f));
         final boolean                    hasReachedMinimumClasses = false;
@@ -86,7 +86,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void fail_given_different_exam_grades_but_not_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades = List.of(
             new Pair<>(10, 4f),
@@ -106,14 +106,11 @@ public class StudentGradeCalculatorShould {
     }
 
     @Test
-    void fail_given_odd_exam_grades_but_not_attending_the_minimum_classes() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+    void fail_given_odd_exam_grades_but_attending_the_minimum_classes() {
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
-        final List<Pair<Integer, Float>> examsGrades = List.of(
-            new Pair<>(50, 5f),
-            new Pair<>(50, 4f)
-        );
-        final boolean hasReachedMinimumClasses = false;
+        final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 5f));
+        final boolean                    hasReachedMinimumClasses = false;
 
         assertEquals(0, studentGradeCalculator.calculateGrades(examsGrades, hasReachedMinimumClasses));
     }
@@ -122,7 +119,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void validate_all_exam_grades_weight_below_100() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades = List.of(
             new Pair<>(10, 4f),
@@ -135,7 +132,7 @@ public class StudentGradeCalculatorShould {
 
     @Test
     void validate_all_exam_grades_weight_over_100() {
-        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator();
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
 
         final List<Pair<Integer, Float>> examsGrades = List.of(
             new Pair<>(90, 4f),
@@ -144,5 +141,37 @@ public class StudentGradeCalculatorShould {
         final boolean hasReachedMinimumClasses = true;
 
         assertEquals(-1, studentGradeCalculator.calculateGrades(examsGrades, hasReachedMinimumClasses));
+    }
+
+    // hasToRaiseOnePoint
+
+    @Test
+    void not_increase_one_extra_point_if_there_is_not_any_benevolent_teacher_in_the_year_to_calculate_grades() {
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2019);
+
+        final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 9.8f));
+        final boolean                    hasReachedMinimumClasses = true;
+
+        assertEquals(9.8f, studentGradeCalculator.calculateGrades(examsGrades, hasReachedMinimumClasses));
+    }
+
+    @Test
+    void increase_one_extra_point_if_there_is_any_benevolent_teacher_in_the_year_to_calculate_grades() {
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2020);
+
+        final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 5f));
+        final boolean                    hasReachedMinimumClasses = true;
+
+        assertEquals(6, studentGradeCalculator.calculateGrades(examsGrades, hasReachedMinimumClasses));
+    }
+
+    @Test
+    void maintain_10_as_the_maximum_grade_even_if_increasing_one_extra_point() {
+        StudentGradeCalculator studentGradeCalculator = new StudentGradeCalculator(2020);
+
+        final List<Pair<Integer, Float>> examsGrades              = List.of(new Pair<>(100, 9.8f));
+        final boolean                    hasReachedMinimumClasses = true;
+
+        assertEquals(10, studentGradeCalculator.calculateGrades(examsGrades, hasReachedMinimumClasses));
     }
 }
