259664a5c3521052f5727cb83f7af4483ddb0f9b
Javier Ferrer <javier.mailserio@gmail.com>
2020-09-22 13:34:12 +0200

    [PHP] [StepStrategy] Remove other cases implementations rather than the concrete class one

--- examples/php/php-step_strategy-01_base/src/VideoStepType.php
+++ examples/php/php-step_strategy-01_base/src/VideoStepType.php
@@ -20,20 +20,6 @@ final class VideoStepType extends StepType
 
     public function stepEstimatedCompletionMinutes(Step $instance): int
     {
-        $estimation = 0;
-
-        switch ($instance->type->code()) {
-            case StepType::VIDEO_STEP_TYPE:
-                $estimation = $instance->videoDurationInMinutes;
-                break;
-            case StepType::QUIZ_STEP_TYPE:
-                $estimation = QuizStepType::QUIZ_STEP_QUESTION_DURATION_ESTIMATION_IN_MINUTES * count($instance->quizQuestions);
-                break;
-            case StepType::EXERCISE_STEP_TYPE:
-                $estimation = ExerciseStepType::EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES;
-                break;
-        }
-
-        return $estimation;
+        return $instance->videoDurationInMinutes;
     }
 }
