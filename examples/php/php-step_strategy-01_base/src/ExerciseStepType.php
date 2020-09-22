612683b36d82d06dfbba82d40560439ffa5d98c2
Javier Ferrer <javier.mailserio@gmail.com>
2020-09-22 13:29:43 +0200

    [PHP] [StepStrategy] Move method to StepType hierarchy

--- examples/php/php-step_strategy-01_base/src/ExerciseStepType.php
+++ examples/php/php-step_strategy-01_base/src/ExerciseStepType.php
@@ -6,6 +6,8 @@ namespace CodelyTv\StepStrategy;
 
 final class ExerciseStepType extends StepType
 {
+    public const EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES = 30;
+
     public function code(): int
     {
         return StepType::EXERCISE_STEP_TYPE;
--- examples/php/php-step_strategy-01_base/src/Step.php
+++ examples/php/php-step_strategy-01_base/src/Step.php
@@ -6,13 +6,10 @@ namespace CodelyTv\StepStrategy;
 
 final class Step
 {
-    private const EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES = 30;
-    private const QUIZ_STEP_QUESTION_DURATION_ESTIMATION_IN_MINUTES = 3;
-
     private string   $title;
-    private StepType $type;
-    private ?int     $videoDurationInMinutes;
-    private ?array   $quizQuestions;
+    public StepType $type;
+    public ?int     $videoDurationInMinutes;
+    public ?array   $quizQuestions;
 
     public function __construct(string $title, int $typeCode, ?int $videoDurationInMinutes, ?array $quizQuestions)
     {
@@ -24,25 +21,6 @@ final class Step
 
     public function estimatedCompletionMinutes(): int
     {
-        return $this->stepEstimatedCompletionMinutes();
-    }
-
-    public function stepEstimatedCompletionMinutes()
-    {
-        $estimation = 0;
-
-        switch ($this->type->code()) {
-            case StepType::VIDEO_STEP_TYPE:
-                $estimation = $this->videoDurationInMinutes;
-                break;
-            case StepType::QUIZ_STEP_TYPE:
-                $estimation = self::QUIZ_STEP_QUESTION_DURATION_ESTIMATION_IN_MINUTES * count($this->quizQuestions);
-                break;
-            case StepType::EXERCISE_STEP_TYPE:
-                $estimation = self::EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES;
-                break;
-        }
-
-        return $estimation;
+        return $this->type->stepEstimatedCompletionMinutes($this);
     }
 }
