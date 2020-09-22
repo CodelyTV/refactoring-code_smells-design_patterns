2fefa681f885f7a4c9825db587cb89b57528588f
Javier Ferrer <javier.mailserio@gmail.com>
2020-09-22 11:22:06 +0200

    [PHP] [User Modelling Example] Replace Data Value with Object for Locale without modifying its API

---
+++ examples/php/php-user_modelling-02_value_objects/src/Locale.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodelyTv\UserModelling;
+
+final class Locale
+{
+    private const SPANISH_LANGUAGE = "es";
+
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function understandSpanish(): bool
+    {
+        $language = substr($this->value, 0, 2);
+
+        return $language === self::SPANISH_LANGUAGE;
+    }
+}
--- examples/php/php-user_modelling-02_value_objects/src/User.php
+++ examples/php/php-user_modelling-02_value_objects/src/User.php
@@ -6,22 +6,18 @@ namespace CodelyTv\UserModelling;
 
 final class User
 {
-    private const SPANISH_LANGUAGE = "es";
-
-    private string $locale;
+    private Locale $locale;
     private Age $age;
 
     public function __construct(string $locale, Age $age)
     {
-        $this->locale = $locale;
+        $this->locale = new Locale($locale);
         $this->age    = $age;
     }
 
     public function understandSpanish(): bool
     {
-        $language = substr($this->locale, 0, 2);
-
-        return $language === self::SPANISH_LANGUAGE;
+        return $this->locale->understandSpanish();
     }
 
     public function isUnderage(): bool
