e97298cb495621dc4b824f9aa2b5e783a8894f56
Javier Ferrer <javier.mailserio@gmail.com>
2020-10-07 11:08:56 +0200

    [PHP] [StepStrategy] Set initial code in its own folder in order to allow other refactoring approaches in subsequent folders linking to them

--- examples/php/php-step_strategy-01_base/src/Step.php
+++ examples/php/php-step_strategy-01_base/src/Step.php
@@ -6,17 +6,34 @@ namespace CodelyTv\StepStrategy;
 
 final class Step
 {
-    private string   $title;
-    private StepType $type;
+    private const EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES = 30;
+    private const QUIZ_STEP_QUESTION_DURATION_ESTIMATION_IN_MINUTES = 3;
 
-    public function __construct(string $title, int $typeCode, ?int $videoDurationInMinutes, ?array $quizQuestions)
+    private string $title;
+    private int    $type; // 0: Video, 1: Quizz, 2: Exercise
+    private ?int    $videoDurationInMinutes;
+    private ?array  $quizQuestions;
+
+    public function __construct(string $title, int $type, ?int $videoDurationInMinutes, ?array $quizQuestions)
     {
-        $this->title = $title;
-        $this->type  = StepType::fromPrimitive($typeCode, $videoDurationInMinutes, $quizQuestions);
+        $this->title                  = $title;
+        $this->type                   = $type;
+        $this->videoDurationInMinutes = $videoDurationInMinutes;
+        $this->quizQuestions          = $quizQuestions;
     }
 
     public function estimatedCompletionMinutes(): int
     {
-        return $this->type->estimatedCompletionMinutes();
+        $estimation = 0;
+
+        if ($this->type === 0) { // Video
+            $estimation = $this->videoDurationInMinutes;
+        } elseif ($this->type === 1) { // Quizz
+            $estimation = self::QUIZ_STEP_QUESTION_DURATION_ESTIMATION_IN_MINUTES * count($this->quizQuestions);
+        } elseif ($this->type === 2) { // Exercise
+            $estimation = self::EXERCISE_STEP_DURATION_ESTIMATION_IN_MINUTES;
+        }
+
+        return $estimation;
     }
 }
