8895eb268c6dbe0dfbf368cb3ede2d3389889e0a
Javier Ferrer <javier.mailserio@gmail.com>
2020-10-07 11:13:02 +0200

    [PHP] [StepStrategy] Start using the StepType hirearchy

--- examples/php/php-step_strategy-02_replace_type_code_with_strategy/src/ExerciseStepType.php
+++ examples/php/php-step_strategy-02_replace_type_code_with_strategy/src/ExerciseStepType.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CodelyTv\StepStrategy;
 
-final class ExerciseStepType
+final class ExerciseStepType extends StepType
 {
     public function code(): int
     {
--- examples/php/php-step_strategy-02_replace_type_code_with_strategy/src/QuizStepType.php
+++ examples/php/php-step_strategy-02_replace_type_code_with_strategy/src/QuizStepType.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CodelyTv\StepStrategy;
 
-final class QuizStepType
+final class QuizStepType extends StepType
 {
     private array $quizQuestions;
 
