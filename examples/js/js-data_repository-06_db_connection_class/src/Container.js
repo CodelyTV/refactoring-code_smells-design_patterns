e342c6362357481aeb839f719a30f3cf02dee9a1
Rafa Gómez <rgomezcasas@gmail.com>
2020-10-07 10:45:43 +0200

    [js][data_repository][06_db_connection_class] Extract connection class

--- examples/js/js-data_repository-06_db_connection_class/src/Container.js
+++ examples/js/js-data_repository-06_db_connection_class/src/Container.js
@@ -3,7 +3,17 @@ import {UsersSearcher}       from "./UsersSearcher";
 import {MySqlConfig}         from "./MySqlConfig";
 
 export class Container {
-    mysqlConfig         = new MySqlConfig();
-    mysqlUserRepository = new MySqlUserRepository(this.mysqlConfig);
+    Mysql = require('sync-mysql');
+    
+    mysqlConfig     = new MySqlConfig();
+    mySqlConnection = new this.Mysql({
+        host:     this.mysqlConfig.host,
+        user:     this.mysqlConfig.user,
+        password: this.mysqlConfig.password,
+        database: this.mysqlConfig.database,
+        port:     this.mysqlConfig.port
+    });
+    
+    mysqlUserRepository = new MySqlUserRepository(this.mySqlConnection);
     usersSearcher       = new UsersSearcher(this.mysqlUserRepository);
 }
--- examples/js/js-data_repository-06_db_connection_class/src/MySqlUserRepository.js
+++ examples/js/js-data_repository-06_db_connection_class/src/MySqlUserRepository.js
@@ -1,13 +1,6 @@
 export class MySqlUserRepository {
-    constructor(config) {
-        const Mysql     = require('sync-mysql');
-        this.connection = new Mysql({
-            host: config.host,
-            user: config.user,
-            password: config.password,
-            database: config.database,
-            port: config.port
-        });
+    constructor(connection) {
+        this.connection = connection;
     }
 
     searchAll() {
