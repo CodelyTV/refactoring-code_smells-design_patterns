dc7032967647a8fe27cabfb64da33bd9943497eb
Rafa Gómez <rgomezcasas@gmail.com>
2020-11-02 17:48:41 +0100

    [php][user_email-02_extract_method] Extract guard clauses

--- examples/php/php-user_email-02_extract_method/src/Controller/UserController.php
+++ examples/php/php-user_email-02_extract_method/src/Controller/UserController.php
@@ -11,18 +11,36 @@ final class UserController
 {
     public function post(string $emailAddress): User
     {
+        $this->ensureEmailIsNotEmpty($emailAddress);
+        $this->ensureEmailIsFormattedCorrectly($emailAddress);
+        $this->ensureEmailHasCommonProvider($emailAddress);
+
+        return new User($emailAddress);
+    }
+
+    private function ensureEmailIsNotEmpty(string $emailAddress): void
+    {
         if ('' === $emailAddress) {
             throw new InvalidArgumentException('The email address is empty');
         }
+    }
 
+    private function ensureEmailIsFormattedCorrectly(string $emailAddress): void
+    {
         if (!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
             throw new InvalidArgumentException("The email address <$emailAddress> is not valid");
         }
+    }
 
-        if (!(strpos($emailAddress, '@yahoo') || strpos($emailAddress, '@gmail') || strpos($emailAddress, '@outlook'))) {
+    private function ensureEmailHasCommonProvider(string $emailAddress): void
+    {
+        if (!$this->emailHasCommonProvider($emailAddress)) {
             throw new InvalidArgumentException("The email address <$emailAddress> has not a common provider");
         }
+    }
 
-        return new User($emailAddress);
+    private function emailHasCommonProvider(string $emailAddress): bool
+    {
+        return strpos($emailAddress, '@yahoo') || strpos($emailAddress, '@gmail') || strpos($emailAddress, '@outlook');
     }
 }
