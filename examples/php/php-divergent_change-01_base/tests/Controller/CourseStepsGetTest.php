18cb51877f3910fc4c6e71500297519517bc19db
Dani Santamaría <santaka87@gmail.com>
2021-04-13 06:48:50 +0200

    [php][divergent_change-01_base] Add test for step validation

--- examples/php/php-divergent_change-01_base/tests/Controller/CourseStepsGetTest.php
+++ examples/php/php-divergent_change-01_base/tests/Controller/CourseStepsGetTest.php
@@ -54,4 +54,27 @@ final class CourseStepsGetTest extends TestCase
         $expected = '[{"id":"uuid","type":"video","duration":14.3,"points":1430},{"id":"uuid","type":"quiz","duration":2.5,"points":25}]';
         self::assertSame($expected, $results);
     }
+
+    /** @test */
+    public function shouldIgnoreStepsWithInvalidType(): void
+    {
+        $platform = $this->createMock(Platform::class);
+        $platform
+            ->method('findCourseSteps')
+            ->with('73D74817-CC25-477D-BF3E-36130087293F')
+            ->willReturn([
+                [
+                    'id' => 'uuid',
+                    'type' => 'survey',
+                    'questions' => null,
+                    'duration' => 13
+                ]
+            ]);
+
+        $sut = new CourseStepsGetController($platform);
+
+        $results = $sut->get('73D74817-CC25-477D-BF3E-36130087293F');
+
+        self::assertSame('[]', $results);
+    }
 }
