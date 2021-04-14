0b1b2f845cde18098673a1480df81d18f3a6cc0e
Rafa Gómez <rgomezcasas@gmail.com>
2021-04-14 12:40:17 +0200

    ESTE ES UN STEP TEMPORAL

--- examples/php/php-divergent_change-01_base/src/Controller/CourseStepsGetController.php
+++ examples/php/php-divergent_change-01_base/src/Controller/CourseStepsGetController.php
@@ -79,6 +79,60 @@ final class CourseStepsGetController
 
         $results .= ']';
 
+        $results = '[';
+
+        $csvLines = explode(PHP_EOL, $csv);
+
+        foreach ($csvLines as $index => $row) {
+            $row = str_getcsv($row);
+
+            if (empty($csv)) {
+                continue;
+            }
+
+            [$stepId, $type, $quizTotalQuestions, $videoDuration] = $row;
+
+            $stepDurationInMinutes = 0;
+            $points                = 0;
+
+            if ($type === self::STEP_TYPE_VIDEO) {
+                $stepDurationInMinutes = $videoDuration * self::VIDEO_DURATION_PAUSES_MULTIPLIER;
+            }
+
+            if ($type === self::STEP_TYPE_QUIZ) {
+                $stepDurationInMinutes = $quizTotalQuestions * self::QUIZ_TIME_PER_QUESTION_MULTIPLIER;
+            }
+
+            if ($type !== self::STEP_TYPE_VIDEO && $type !== self::STEP_TYPE_QUIZ) {
+                continue;
+            }
+
+            if ($type === self::STEP_TYPE_VIDEO) {
+                $points = $stepDurationInMinutes * self::VIDEO_POINTS_PER_MINUTE;
+            }
+
+            if ($type === self::STEP_TYPE_QUIZ) {
+                $points = $stepDurationInMinutes * self::QUIZ_POINTS_PER_MINUTE;
+            }
+
+            $results .= json_encode(
+                [
+                    'id'       => $stepId,
+                    'type'     => $type,
+                    'duration' => $stepDurationInMinutes,
+                    'points'   => $points,
+                ],
+                JSON_THROW_ON_ERROR
+            );
+
+            $hasMoreRows = $index !== count($csvLines) - 1;
+            if ($hasMoreRows) {
+                $results .= ',';
+            }
+        }
+
+        $results .= ']';
+
         return $results;
     }
 }
