f96cfbee9bb195396fb887218483746e5c5c46d7
ismanapa@gmail.com <ismanapa@gmail.com>
2021-04-26 12:12:45 +0200

    [csharp] [courses_steps_csv] Implement 01 base

---
+++ examples/csharp/csharp-courses_steps_csv-01_base/src/CoursesStepsCsv/Steps.cs
@@ -0,0 +1,11 @@
+
+namespace CodelyTv.CoursesStepsCsv
+{
+    public sealed class Step
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public double Duration { get; set; }
+        public double Points { get; set; }
+    }
+}
--- examples/csharp/csharp-courses_steps_csv-01_base/test/CoursesStepsCsv.Tests/CourseStepsGetControllerShould.cs
+++ examples/csharp/csharp-courses_steps_csv-01_base/test/CoursesStepsCsv.Tests/CourseStepsGetControllerShould.cs
@@ -24,7 +24,35 @@ namespace CodelyTv.CoursesStepsCsv.Tests
 
             var actualCourseSteps = courseStepsGetController.Get(courseId);
 
-            Assert.Equal("", actualCourseSteps);
+            Assert.Equal("[]", actualCourseSteps);
+        }
+
+        [Fact]
+        public void ReturnExistingCourseSteps()
+        {
+            var courseId = "8fe17ce6-1d33-4b6b-a27c-4e0d1f870a19";
+            var csv = String.Join(
+                Environment.NewLine,
+                "1,video,,13",
+                "2,quiz,5,");
+            GivenPlatformReturnsCourseStepCsv(courseId, csv);
+
+            var results = courseStepsGetController.Get(courseId);
+
+            var expected = "[{\"id\":\"1\",\"type\":\"video\",\"duration\":14.3,\"points\":1430},{\"id\":\"2\",\"type\":\"quiz\",\"duration\":2.5,\"points\":25}]";
+            Assert.Equal(expected, results);
+        }
+
+        [Fact]
+        public void IgnoreStepsWithInvalidType()
+        {
+            var courseId = "8fe17ce6-1d33-4b6b-a27c-4e0d1f870a19";
+            var csv = "1,survey,,13";
+            GivenPlatformReturnsCourseStepCsv(courseId, csv);
+
+            var results = courseStepsGetController.Get(courseId);
+
+            Assert.Equal("[]", results);
         }
 
         private void GivenPlatformReturnsCourseStepCsv(string courseId, string csv)
