0042153fab3d00e9eba2886a7a26a4b57e8e442c
Joel Coll Costa <joel.coll@gmail.com>
2021-04-30 19:55:51 +0200

    Extract validNormalizedPower to a private attribute of Contract class

--- examples/typescript/ts-energy_power-01_base/src/Domain/Contract.ts
+++ examples/typescript/ts-energy_power-01_base/src/Domain/Contract.ts
@@ -1,6 +1,18 @@
 import {InvalidPower} from "./InvalidPower";
 
 export class Contract {
+    private static validNormalizedPowers: Array<number> = [
+        1150,
+        1725,
+        2300,
+        3450,
+        4600,
+        5750,
+        6900,
+        8050,
+        9200
+    ]
+
     constructor(
         private id: string,
         private contractedPower: number
@@ -13,19 +25,7 @@ export class Contract {
     }
 
     public static ensurePowerIsNormalized(newPower: number) {
-        const validNormalizedPowers: Array<number> = [
-            1150,
-            1725,
-            2300,
-            3450,
-            4600,
-            5750,
-            6900,
-            8050,
-            9200
-        ]
-
-        if (!validNormalizedPowers.includes(newPower)) {
+        if (!this.validNormalizedPowers.includes(newPower)) {
             throw new InvalidPower(newPower)
         }
     }
