a6e256e80ee2544fff98873aaf7b5ddd1401c0f1
Dani Santamaría <santaka87@gmail.com>
2021-08-31 12:57:25 +0200

    [ts] [stock_counter_dynamic] [02_extract_parameterized_factory_method] inline variable

--- examples/ts/ts-stock_counter_dynamic-02_extract_parameterized_factory_method/src/index.ts
+++ examples/ts/ts-stock_counter_dynamic-02_extract_parameterized_factory_method/src/index.ts
@@ -1,15 +1,11 @@
 import {getWarehouseProductFeed} from "./getWarehoseProductFeed";
 import {FeedStockCounter} from "./FeedStockCounter";
-import {FeedParserCsv} from "./FeedParserCsv";
-import {FeedParserJson} from "./FeedParserJson";
-import {FeedParser} from "./FeedParser";
 import {FeedParserFromContentType} from "./FeedParserFromContentType";
 
 const feed = getWarehouseProductFeed();
 
-let contentType = feed.contentType;
 const feedParserFromContentType = new FeedParserFromContentType();
-const parser = feedParserFromContentType.get(contentType);
+const parser = feedParserFromContentType.get(feed.contentType);
 const feedStockCounter = new FeedStockCounter(parser);
 const totalStock = feedStockCounter.totalStock(feed);
 
