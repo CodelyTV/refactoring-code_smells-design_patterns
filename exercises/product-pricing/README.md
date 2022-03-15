fa93f8d19fa0fa642a4ecbf0898ef2cc94a2c024
Dani Santamaría <santaka87@gmail.com>
2022-03-15 09:45:08 +0100

    feat(product-pricing): rename exercise folder

---
+++ exercises/product-pricing/README.md
@@ -0,0 +1,17 @@
+# Product pricing
+
+Introductory TDD exercise.
+
+## User story description
+
+We need to sell products in our online platform,
+so we have to calculate the total price of them.
+
+The total price of a product that we are going to
+show to the customer must have the 21% VAT included.
+
+## Exercise
+
+Available languages:
+
+- [TypeScript](base/typescript/README.md)
---
+++ exercises/product-pricing/base/typescript/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
