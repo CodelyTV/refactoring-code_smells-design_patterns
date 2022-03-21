a1cbb8153f1c4a4ee993c762d32b6b407ef54672
Dani Santamaría <santaka87@gmail.com>
2022-03-21 12:17:39 +0100

    feat(graduated-tiered-pricing): [tpp] 🔵 generalization preparatory refactoring

--- exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
+++ exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
@@ -1,17 +1,18 @@
 export class GraduatedTieredPricing {
   for(subscriptions: number): number {
     const tier1total = 299 + 299;
+    const tier1size = 2;
 
     if (subscriptions == 5) {
-      return tier1total + 239 + 239 + 239;
+      return tier1total + (subscriptions - tier1size) * 239;
     }
 
     if (subscriptions == 4) {
-      return tier1total + 239 + 239;
+      return tier1total + (subscriptions - tier1size) * 239;
     }
 
     if (subscriptions == 3) {
-      return tier1total + 239;
+      return tier1total + (subscriptions - tier1size) * 239;
     }
 
     return subscriptions * 299;
