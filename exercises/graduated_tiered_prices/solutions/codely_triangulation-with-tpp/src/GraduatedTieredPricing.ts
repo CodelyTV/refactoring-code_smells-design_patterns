a0588424576d5a776c40663e41f686251f6137ea
Dani Santamaría <santaka87@gmail.com>
2022-03-21 12:26:38 +0100

    feat(graduated-tiered-pricing): [tpp] 🔵 decompose constante and extract variables

--- exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
+++ exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
@@ -1,14 +1,19 @@
 export class GraduatedTieredPricing {
   for(subscriptions: number): number {
-    const tier1total = 299 + 299;
     const tier1size = 2;
+    const tier1price = 299;
+    const tier1total = tier1size * tier1price;
+    const tier2size = 8;
+    const tier2price = 239;
+    const tier2total = tier2size * tier2price;
+    const tier3price = 219;
 
     if (subscriptions == 12) {
       return 2948;
     }
 
     if (subscriptions == 11) {
-      return 2729;
+      return tier1total + tier2total + tier3price;
     }
 
     if (subscriptions >= 3) {
