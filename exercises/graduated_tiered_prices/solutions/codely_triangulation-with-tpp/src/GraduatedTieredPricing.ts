0626821860977426a26a849c8d9f2b5097898849
Dani Santamaría <santaka87@gmail.com>
2022-03-21 12:22:00 +0100

    feat(graduated-tiered-pricing): [tpp] 🟢 5. statement → statements

--- exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
+++ exercises/graduated_tiered_prices/solutions/codely_triangulation-with-tpp/src/GraduatedTieredPricing.ts
@@ -3,6 +3,10 @@ export class GraduatedTieredPricing {
     const tier1total = 299 + 299;
     const tier1size = 2;
 
+    if (subscriptions == 11) {
+      return 2729;
+    }
+
     if (subscriptions >= 3) {
       return tier1total + (subscriptions - tier1size) * 239;
     }
