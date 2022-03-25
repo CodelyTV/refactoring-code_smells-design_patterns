c08e48f8997a06ce8660588fd0456b2597df0e02
Isaac Abreu Rodríguez <caasi1024@gmail.com>
2022-03-25 22:43:39 +0100

    🔵 Refactor subscription algorithm

--- exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
+++ exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
@@ -5,11 +5,10 @@ fn get_total_subscription_price(number_of_subscriptions: u32) -> u32 {
     const FOURTH_UNIT_PRICE: u32 = 199;
 
     match number_of_subscriptions {
-        26 => FOURTH_UNIT_PRICE * number_of_subscriptions,
-        50 => FOURTH_UNIT_PRICE * number_of_subscriptions,
         1|2 => FIRST_UNIT_PRICE * number_of_subscriptions,
         3..=10 => SECOND_UNIT_PRICE * number_of_subscriptions,
-        _ => THIRD_UNIT_PRICE * number_of_subscriptions,
+        11..=25 => THIRD_UNIT_PRICE * number_of_subscriptions,
+        _ => FOURTH_UNIT_PRICE * number_of_subscriptions,
     }
 }
 
