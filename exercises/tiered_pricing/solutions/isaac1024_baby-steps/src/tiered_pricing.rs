3b9785da4e84668b3e414ae7743db9744cbf70b0
Isaac Abreu Rodríguez <caasi1024@gmail.com>
2022-03-25 22:27:55 +0100

    🔵 Extract the greatest common division for 11 and 25 subscriptions

--- exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
+++ exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
@@ -3,8 +3,8 @@ fn get_total_subscription_price(number_of_subscriptions: u32) -> u32 {
     const SECOND_UNIT_PRICE: u32 = 239;
 
     match number_of_subscriptions {
-        11 => 2409,
-        25 => 5475,
+        11 => 219 * number_of_subscriptions,
+        25 => 219 * number_of_subscriptions,
         1|2 => FIRST_UNIT_PRICE * number_of_subscriptions,
         _ => SECOND_UNIT_PRICE * number_of_subscriptions
     }
