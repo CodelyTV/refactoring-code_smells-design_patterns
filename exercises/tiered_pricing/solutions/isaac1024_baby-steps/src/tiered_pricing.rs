69220f8925821eff20061cf9f92e7659f8489e1a
Isaac Abreu Rodríguez <caasi1024@gmail.com>
2022-03-25 22:41:29 +0100

    🔵 Extract magic number to constant

--- exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
+++ exercises/tiered_pricing/solutions/isaac1024_baby-steps/src/tiered_pricing.rs
@@ -2,10 +2,11 @@ fn get_total_subscription_price(number_of_subscriptions: u32) -> u32 {
     const FIRST_UNIT_PRICE: u32 = 299;
     const SECOND_UNIT_PRICE: u32 = 239;
     const THIRD_UNIT_PRICE: u32 = 219;
+    const FOURTH_UNIT_PRICE: u32 = 199;
 
     match number_of_subscriptions {
-        26 => 199 * number_of_subscriptions,
-        50 => 199 * number_of_subscriptions,
+        26 => FOURTH_UNIT_PRICE * number_of_subscriptions,
+        50 => FOURTH_UNIT_PRICE * number_of_subscriptions,
         1|2 => FIRST_UNIT_PRICE * number_of_subscriptions,
         3..=10 => SECOND_UNIT_PRICE * number_of_subscriptions,
         _ => THIRD_UNIT_PRICE * number_of_subscriptions,
