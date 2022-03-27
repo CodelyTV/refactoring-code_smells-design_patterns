347e7b96fe09b9771c193f5335d8d9deb05d7494
mperezi <mperezibars@gmail.com>
2022-03-27 09:43:56 +0200

    Refactor(prod): Discover (first, second) Tier

--- exercises/tiered_pricing/solutions/mperezi/src/main/java/tv/codely/checkout/TieredPricing.java
+++ exercises/tiered_pricing/solutions/mperezi/src/main/java/tv/codely/checkout/TieredPricing.java
@@ -2,12 +2,13 @@ package tv.codely.checkout;
 
 public class TieredPricing {
 
-  private static final double UNIT_PRICE = 299.0;
+  private static final double FIRST_TIER_UNIT_PRICE = 299.0;
+  private static final double SECOND_TIER_UNIT_PRICE = 239.0;
 
   public double totalPrice(int subscriptions) {
     if (subscriptions < 3) {
-      return subscriptions * UNIT_PRICE;
+      return subscriptions * FIRST_TIER_UNIT_PRICE;
     }
-    return 717.0;
+    return subscriptions * SECOND_TIER_UNIT_PRICE;
   }
 }
