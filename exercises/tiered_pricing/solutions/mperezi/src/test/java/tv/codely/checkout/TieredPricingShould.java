fec246c71a0afac9d12828d778fca8703b82f452
mperezi <mperezibars@gmail.com>
2022-03-27 09:43:57 +0200

    Refactor(test): Refactor test method for first tiers

--- exercises/tiered_pricing/solutions/mperezi/src/test/java/tv/codely/checkout/TieredPricingShould.java
+++ exercises/tiered_pricing/solutions/mperezi/src/test/java/tv/codely/checkout/TieredPricingShould.java
@@ -2,15 +2,21 @@ package tv.codely.checkout;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import javax.util.streamex.StreamEx;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TieredPricingShould {
+  private static final IntStream FIRST_TIER_RANGE  = IntStream.rangeClosed(1, 2);
+  private static final IntStream SECOND_TIER_RANGE = IntStream.rangeClosed(3, 10);
+  private static final IntStream THIRD_TIER_RANGE  = IntStream.rangeClosed(11, 25);
+  private static final IntStream FOURTH_TIER_RANGE = IntStream.rangeClosed(26, 50);
 
   private TieredPricing tieredPricing;
 
@@ -20,35 +26,11 @@ public class TieredPricingShould {
   }
 
   @ParameterizedTest
-  @MethodSource("rangeFirstTier")
-  void returnTotalPriceForFirstTier(int subscriptions) {
+  @MethodSource("tierRanges")
+  void returnTotalPriceForEachTier(int subscriptions, double unitPrice) {
     double totalPrice = tieredPricing.totalPrice(subscriptions);
 
-    assertThat(totalPrice).isEqualTo(subscriptions * 299.0);
-  }
-
-  @ParameterizedTest
-  @MethodSource("rangeSecondTier")
-  void returnTotalPriceForSecondTier(int subscriptions) {
-    double totalPrice = tieredPricing.totalPrice(subscriptions);
-
-    assertThat(totalPrice).isEqualTo(subscriptions * 239.0);
-  }
-
-  @ParameterizedTest
-  @MethodSource("rangeThirdTier")
-  void returnTotalPriceForThirdTier(int subscriptions) {
-    double totalPrice = tieredPricing.totalPrice(subscriptions);
-
-    assertThat(totalPrice).isEqualTo(subscriptions * 219.0);
-  }
-
-  @ParameterizedTest
-  @MethodSource("rangeFourthTier")
-  void returnTotalPriceForFourthTier(int subscriptions) {
-    double totalPrice = tieredPricing.totalPrice(subscriptions);
-
-    assertThat(totalPrice).isEqualTo(subscriptions * 199.0);
+    assertThat(totalPrice).isEqualTo(subscriptions * unitPrice);
   }
 
   @ParameterizedTest
@@ -59,19 +41,14 @@ public class TieredPricingShould {
     assertThat(totalPrice).isEqualTo(subscriptions * 149.0);
   }
 
-  private static Stream<Integer> rangeFirstTier() {
-    return IntStream.rangeClosed(1, 2).boxed();
-  }
-
-  private static Stream<Integer> rangeSecondTier() {
-    return IntStream.rangeClosed(3, 10).boxed();
-  }
-
-  private static Stream<Integer> rangeThirdTier() {
-    return IntStream.rangeClosed(11, 25).boxed();
-  }
-
-  private static Stream<Integer> rangeFourthTier() {
-    return IntStream.rangeClosed(26, 50).boxed();
+  private static Stream<Arguments> tierRanges() {
+    Stream<Arguments> firstTier  = FIRST_TIER_RANGE.mapToObj(i -> Arguments.of(i, 299.0));
+    Stream<Arguments> secondTier = SECOND_TIER_RANGE.mapToObj(i -> Arguments.of(i, 239.0));
+    Stream<Arguments> thirdTier  = THIRD_TIER_RANGE.mapToObj(i -> Arguments.of(i, 219.0));
+    Stream<Arguments> fourthTier = FOURTH_TIER_RANGE.mapToObj(i -> Arguments.of(i, 199.0));
+    return StreamEx.of(firstTier)
+                   .append(secondTier)
+                   .append(thirdTier)
+                   .append(fourthTier);
   }
 }
