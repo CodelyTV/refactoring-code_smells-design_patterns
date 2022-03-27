1518821bb8978f084096625108738a323cc1a19b
mperezi <mperezibars@gmail.com>
2022-03-27 09:43:56 +0200

    Refactor(test): Parameterize subscriptions

--- exercises/tiered_pricing/solutions/mperezi/src/test/java/tv/codely/checkout/TieredPricingShould.java
+++ exercises/tiered_pricing/solutions/mperezi/src/test/java/tv/codely/checkout/TieredPricingShould.java
@@ -1,7 +1,11 @@
 package tv.codely.checkout;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,31 +18,39 @@ public class TieredPricingShould {
     tieredPricing = new TieredPricing();
   }
 
-  @Test
-  void returnFirstTierUnitPriceForOneSubscription() {
-    double totalPrice = tieredPricing.totalPrice(1);
+  @ParameterizedTest
+  @MethodSource("rangeFirstTier")
+  void returnTotalPriceForFirstTier(int subscriptions) {
+    double totalPrice = tieredPricing.totalPrice(subscriptions);
 
-    assertThat(totalPrice).isEqualTo(299.0);
+    assertThat(totalPrice).isEqualTo(subscriptions * 299.0);
   }
 
-  @Test
-  void returnDoubleTheFirstTierUnitPriceForTwoSubscriptions() {
-    double totalPrice = tieredPricing.totalPrice(2);
+  @ParameterizedTest
+  @MethodSource("rangeSecondTier")
+  void returnTotalPriceForSecondTier(int subscriptions) {
+    double totalPrice = tieredPricing.totalPrice(subscriptions);
 
-    assertThat(totalPrice).isEqualTo(598.0);
+    assertThat(totalPrice).isEqualTo(subscriptions * 239.0);
   }
 
-  @Test
-  void returnTripleTheSecondTierUnitPriceForThreeSubscriptions() {
-    double totalPrice = tieredPricing.totalPrice(3);
+  @ParameterizedTest
+  @MethodSource("rangeThirdTier")
+  void returnTotalPriceForThirdTier(int subscriptions) {
+    double totalPrice = tieredPricing.totalPrice(subscriptions);
 
-    assertThat(totalPrice).isEqualTo(717.0);
+    assertThat(totalPrice).isEqualTo(subscriptions * 219.0);
   }
 
-  @Test
-  void returnElevenTimesTheThirdTierUnitPriceForElevenSubscriptions() {
-    double totalPrice = tieredPricing.totalPrice(11);
+  private static Stream<Integer> rangeFirstTier() {
+    return IntStream.rangeClosed(1, 2).boxed();
+  }
 
-    assertThat(totalPrice).isEqualTo(2409.0);
+  private static Stream<Integer> rangeSecondTier() {
+    return IntStream.rangeClosed(3, 10).boxed();
+  }
+
+  private static Stream<Integer> rangeThirdTier() {
+    return IntStream.rangeClosed(11, 25).boxed();
   }
 }
