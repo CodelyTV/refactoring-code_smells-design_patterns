0687fd0e3ea970c603f55541e259bbcc3f741565
mperezi <mperezibars@gmail.com>
2022-03-27 09:43:56 +0200

    Refactor: Push tier selection logic deep inside

--- exercises/tiered_pricing/solutions/mperezi/src/main/java/tv/codely/checkout/TieredPricing.java
+++ exercises/tiered_pricing/solutions/mperezi/src/main/java/tv/codely/checkout/TieredPricing.java
@@ -1,16 +1,29 @@
 package tv.codely.checkout;
 
+import java.util.Arrays;
+
 public class TieredPricing {
 
   private enum Tier {
-    FIRST(299.0),
-    SECOND(239.0),
-    THIRD(219.0);
+    FIRST(299.0, 1, 2),
+    SECOND(239.0, 3, 10),
+    THIRD(219.0, 11, 25);
 
     final double unitPrice;
+    final int startRange;
+    final int endRange;
 
-    Tier(double unitPrice) {
+    Tier(double unitPrice, int startRange, int endRange) {
       this.unitPrice = unitPrice;
+      this.startRange = startRange;
+      this.endRange = endRange;
+    }
+
+    static Tier of(int subscriptions) {
+      return Arrays.stream(values())
+                   .filter(tier -> subscriptions >= tier.startRange && subscriptions <= tier.endRange)
+                   .findFirst()
+                   .orElseThrow(() -> new IllegalArgumentException("Invalid number of subscriptions: " + subscriptions));
     }
   }
 
@@ -19,12 +32,6 @@ public class TieredPricing {
   }
 
   private Tier getTier(int subscriptions) {
-    if (subscriptions < 3) {
-      return Tier.FIRST;
-    }
-    if (subscriptions < 11) {
-      return Tier.SECOND;
-    }
-    return Tier.THIRD;
+    return Tier.of(subscriptions);
   }
 }
