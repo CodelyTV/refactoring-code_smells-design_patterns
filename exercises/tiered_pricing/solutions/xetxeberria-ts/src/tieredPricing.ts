e6f48507ccf7821896ea5488b15b079b2b551e5a
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2022-04-09 18:20:47 +0200

    Refactor: Write if statements in one line each

--- exercises/tiered_pricing/solutions/xetxeberria-ts/src/tieredPricing.ts
+++ exercises/tiered_pricing/solutions/xetxeberria-ts/src/tieredPricing.ts
@@ -6,15 +6,10 @@ export function tieredPricing(licenseAmount: number): number {
 
 function calculateLicensePrice(licenseAmount: number): number {
   let licensePrice = 299;
-  if (licenseAmount >= 3) {
-    licensePrice = 239;
-  }
-  if (licenseAmount >= 11) {
-    licensePrice = 219;
-  }
-  if (licenseAmount >= 26) {
-    licensePrice = 199;
-  }
+
+  if (licenseAmount >= 3) licensePrice = 239;
+  if (licenseAmount >= 11) licensePrice = 219;
+  if (licenseAmount >= 26) licensePrice = 199;
 
   return licensePrice;
 }
