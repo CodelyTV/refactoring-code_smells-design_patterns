90fad78cf8faa02142c4d4a0f46e8705b58cd258
Xabi Etxeberria <xabier.etxeberria93@gmail.com>
2022-04-09 18:21:22 +0200

    Refactor: Make tests more readable
    Write the license amount with numbers instead of letters

--- exercises/tiered_pricing/solutions/xetxeberria-ts/tests/tieredPricing.test.ts
+++ exercises/tiered_pricing/solutions/xetxeberria-ts/tests/tieredPricing.test.ts
@@ -5,19 +5,19 @@ describe("TieredPricing should", () => {
     expect(tieredPricing(1)).toBe(299);
   });
 
-  test("return 598 for two licenses", async () => {
+  test("return 598 for 2 licenses", async () => {
     expect(tieredPricing(2)).toBe(598);
   });
 
-  test("return 717 for three licenses", async () => {
+  test("return 717 for 3 licenses", async () => {
     expect(tieredPricing(3)).toBe(717);
   });
 
-  test("return 2409 for eleven licenses", async () => {
+  test("return 2409 for 11 licenses", async () => {
     expect(tieredPricing(11)).toBe(2409);
   });
 
-  test("return 5373 for twenty seven licenses", async () => {
+  test("return 5373 for 27 licenses", async () => {
     expect(tieredPricing(27)).toBe(5373);
   });
 });
