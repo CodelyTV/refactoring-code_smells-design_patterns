32c46d9048760951fee42c295f0fbe967b5af8a5
Nuria <soriano.nuria@gmail.com>
2022-04-12 17:11:42 +0200

    feat(user-signup): [next-ts-middle-out] 🟢 add form to home

--- exercises/user_signup/solutions/codely_next-typescript-middle-out/src/pages/index.tsx
+++ exercises/user_signup/solutions/codely_next-typescript-middle-out/src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { SignUpForm } from "../components/SignUpForm";
+
 export default function Home(): JSX.Element {
   
   return (
     <>
       <h1>User Signup</h1>
+      <SignUpForm />
     </>
   );
 }
