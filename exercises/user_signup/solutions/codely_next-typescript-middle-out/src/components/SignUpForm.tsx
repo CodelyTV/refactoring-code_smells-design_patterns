24d13bf5409708e6cfc1ad302bb72656e259e354
Nuria <soriano.nuria@gmail.com>
2022-04-12 14:33:24 +0200

    feat(user-signup): [next-ts-middle-out] 🟢 add fake implementation

--- exercises/user_signup/solutions/codely_next-typescript-middle-out/src/components/SignUpForm.tsx
+++ exercises/user_signup/solutions/codely_next-typescript-middle-out/src/components/SignUpForm.tsx
@@ -1,3 +1,25 @@
+import React, { useState } from "react";
+import { Input } from "./Input";
+
+type FormStatus = "success" | "error" | "initial"
+
 export function SignUpForm() {
-  return (<></>)
+  const [formStatus, setFormStatus] = useState<FormStatus>("initial")
+
+  function handleSubmit(ev: React.FormEvent) {
+    ev.preventDefault()
+    setFormStatus("success")
+  }
+
+  if (formStatus === "success") {
+    return (
+      <h1>Thank you!</h1>
+    )
+  }
+
+  return (<form onSubmit={handleSubmit}>
+    <Input label="Name" id="name" />
+    <Input type="email" label="Email" id="email"  />
+    <button type="submit">Submit</button>
+  </form>)
 }
