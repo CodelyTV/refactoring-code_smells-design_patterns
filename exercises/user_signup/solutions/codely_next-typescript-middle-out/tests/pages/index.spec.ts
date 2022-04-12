01c1a5fef74fbeb4c6ed1590123d5bace1533da2
Nuria <soriano.nuria@gmail.com>
2022-04-12 17:15:55 +0200

    feat(user-signup): [next-ts-middle-out] 🔴 add test for invalid form data

--- exercises/user_signup/solutions/codely_next-typescript-middle-out/tests/pages/index.spec.ts
+++ exercises/user_signup/solutions/codely_next-typescript-middle-out/tests/pages/index.spec.ts
@@ -17,4 +17,23 @@ describe("The sign up user form", () => {
 
     successMessage.should("exist");
   });
+
+  it("displays error message on form submission if data is incorrect", () => {
+    cy.visit("/");
+    const name = "J";
+    const email = "jane@gmail.com";
+
+    cy.find
+    cy.findByLabelText(/name/i)
+      .type(name);
+    cy.findByLabelText(/email/i)
+      .type(email);
+
+    cy.findByRole("button", { name: /submit/i })
+      .click()
+
+    const successMessage = cy.findByText(/an error ocurred/i)
+
+    successMessage.should("exist");
+  });
 });
