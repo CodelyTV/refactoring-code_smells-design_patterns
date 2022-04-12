eb26d3412c650d3fc53fe1d7c0932a44bcb3a8db
Nuria <soriano.nuria@gmail.com>
2022-04-12 17:19:12 +0200

    feat(user-signup): [next-ts-middle-out] 🟢 add signUpUser fetch implementation

--- exercises/user_signup/solutions/codely_next-typescript-middle-out/src/services/signUpUser.ts
+++ exercises/user_signup/solutions/codely_next-typescript-middle-out/src/services/signUpUser.ts
@@ -1,3 +1,14 @@
-export async function signUpUser({name, email}: {name: string, email: string}): Promise<void> {
-  return;
+export async function signUpUser(userData: {name: string, email: string}): Promise<void> {
+  await fetch("/api/signup", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then(response => {
+    if (response.status !== 200) {
+      throw new Error()
+    }
+  })
 }
