1c46f297de1ef0c219f750705b0087cf990a34ca
Nuria <soriano.nuria@gmail.com>
2022-04-12 16:51:52 +0200

    feat(user-signup): [next-ts-middle-out] 🔴 add e2e test for form

--- exercises/user_signup/solutions/codely_next-typescript-middle-out/tests/pages/index.spec.ts
+++ exercises/user_signup/solutions/codely_next-typescript-middle-out/tests/pages/index.spec.ts
@@ -1,8 +1,20 @@
-describe("The Home Page", () => {
-  it("works", () => {
-    
-    cy.visit("/")
-      .contains("User Signup")
-      .should("exist");
+describe("The sign up user form", () => {
+  it("displays success message on form submission", () => {
+    cy.visit("/");
+    const name = "Jane Doe";
+    const email = "jane@gmail.com";
+
+    cy.find
+    cy.findByLabelText(/name/i)
+      .type(name);
+    cy.findByLabelText(/email/i)
+      .type(email);
+
+    cy.findByRole("button", { name: /submit/i })
+      .click()
+
+    const successMessage = cy.findByText(/thank you/i)
+
+    successMessage.should("exist");
   });
 });
