da9c61d38b5f5505f7db5c657cef2a04f88b2190
Dani Santamaría <santaka87@gmail.com>
2022-04-20 08:48:15 +0200

    feat(legacy-create-video): [golden-master] extract VideoCreator class

---
+++ exercises/legacy_create_video/solutions/codely_php-symfony_golden-master/src/AppBundle/Application/VideoCreator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Application;
+
+final class VideoCreator
+{
+    public function createVideo($title, $url, $courseId, object $connection): array
+    {
+        $title = $this->sanitizeTitle($title);
+
+        $sql = "INSERT INTO video (title, url, course_id) 
+                VALUES (\"{$title}\",
+                        \"{$url}\",
+                        {$courseId}
+                )";
+
+        // Prepare doctrine statement
+        $stmt = $connection->prepare($sql);
+        $stmt->execute();
+
+        // IMPORTANT: Obtaining the video id. Take care, it's done without another query :)
+        $videoId = $connection->lastInsertId();
+        return array($title, $videoId);
+    }
+
+    private function sanitizeTitle(string $title): string
+    {
+        if (strpos($title, "hexagonal")) {
+            $title = str_replace("hexagonal", "Hexagonal", $title);
+        }
+        if (strpos($title, "solid")) {
+            $title = str_replace("solid", "SOLID", $title);
+        }
+        if (strpos($title, "tdd")) {
+            $title = str_replace("tdd", "TDD", $title);
+        }
+        return $title;
+    }
+}
--- exercises/legacy_create_video/solutions/codely_php-symfony_golden-master/src/AppBundle/Controller/VideoController.php
+++ exercises/legacy_create_video/solutions/codely_php-symfony_golden-master/src/AppBundle/Controller/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Application\VideoCreator;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -10,6 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class VideoController extends BaseController
 {
+    /** @var VideoCreator */
+    private $videoCreator;
+
+    public function __construct()
+    {
+        $this->videoCreator = new VideoCreator();
+    }
+
     /**
      * Method used to create a new video
      * @todo Validate the request
@@ -33,36 +42,8 @@ class VideoController extends BaseController
         ];
     }
 
-    private function sanitizeTitle(string $title): string
-    {
-        if (strpos($title, "hexagonal")) {
-            $title = str_replace("hexagonal", "Hexagonal", $title);
-        }
-        if (strpos($title, "solid")) {
-            $title = str_replace("solid", "SOLID", $title);
-        }
-        if (strpos($title, "tdd")) {
-            $title = str_replace("tdd", "TDD", $title);
-        }
-        return $title;
-    }
-
     private function createVideo($title, $url, $courseId, object $connection): array
     {
-        $title = $this->sanitizeTitle($title);
-
-        $sql = "INSERT INTO video (title, url, course_id) 
-                VALUES (\"{$title}\",
-                        \"{$url}\",
-                        {$courseId}
-                )";
-
-        // Prepare doctrine statement
-        $stmt = $connection->prepare($sql);
-        $stmt->execute();
-
-        // IMPORTANT: Obtaining the video id. Take care, it's done without another query :)
-        $videoId = $connection->lastInsertId();
-        return array($title, $videoId);
+        return $this->videoCreator->createVideo($title, $url, $courseId, $connection);
     }
 }
