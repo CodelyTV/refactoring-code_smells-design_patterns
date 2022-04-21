89bc200b4df352a94027e6cfadb4886354d9310e
Oscar Dario Florez Diaz <oscarflorez1381@gmail.com>
2022-04-20 21:33:48 -0400

    feat(tiered_pricing): 🔴 test for 1 subscription

--- exercises/tiered_pricing/solutions/oflorez/src/test/java/tv/codely/checkout/TieredPricingShould.java
+++ exercises/tiered_pricing/solutions/oflorez/src/test/java/tv/codely/checkout/TieredPricingShould.java
@@ -2,12 +2,13 @@ package tv.codely.checkout;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TieredPricingShould {
 
     @Test
-	void unit_test_example() {
-        assertTrue(true);
+	void return_total_price_299_for_1_subscription() {
+        var pricing = new TieredPricing();
+        assertEquals(299, pricing.calculateTotalPrice(1));
 	}
 }
