30f5d44c32857a82c58da09abf61277fb21f3dd1
Alexander <undergrounder16@gmail.com>
2022-05-24 10:34:22 -0500

    implement obvious implementation

--- exercises/tiered_pricing/solutions/aortiz_typescript/src/Subscription.ts
+++ exercises/tiered_pricing/solutions/aortiz_typescript/src/Subscription.ts
@@ -1,7 +1,25 @@
 export class Subscription {
-  constructor(private units: number) {}
+  private tieredPrice!: number;
+
+  constructor(private units: number) {
+    this.setTieredPrice();
+  }
+
+  private setTieredPrice() {
+    if (this.units < 3) {
+      this.tieredPrice = 299;
+    } else if (this.units < 11) {
+      this.tieredPrice = 239;
+    } else if (this.units < 26) {
+      this.tieredPrice = 219;
+    } else if (this.units < 51) {
+      this.tieredPrice = 199;
+    } else {
+      this.tieredPrice = 149;
+    }
+  }
 
   getTotalPrice() {
-    return 299;
+    return this.tieredPrice * this.units;
   }
 }
--- exercises/tiered_pricing/solutions/aortiz_typescript/tests/Subscription.test.ts
+++ exercises/tiered_pricing/solutions/aortiz_typescript/tests/Subscription.test.ts
@@ -10,4 +10,34 @@ describe("Subscription", () => {
 
     expect(subscription.getTotalPrice()).toBe(299);
   });
+
+  it("can be calculate subscription price of 2 unit", () => {
+    const subscription = new Subscription(1);
+
+    expect(subscription.getTotalPrice()).toBe(598);
+  });
+
+  it("can be calculate subscription price of 10 units", () => {
+    const subscription = new Subscription(10);
+
+    expect(subscription.getTotalPrice()).toBe(2390);
+  });
+
+  it("can be calculate subscription price of 25 units", () => {
+    const subscription = new Subscription(25);
+
+    expect(subscription.getTotalPrice()).toBe(5475);
+  });
+
+  it("can be calculate subscription price of 50 units", () => {
+    const subscription = new Subscription(50);
+
+    expect(subscription.getTotalPrice()).toBe(9950);
+  });
+
+  it("can be calculate subscription price of 68 units", () => {
+    const subscription = new Subscription(68);
+
+    expect(subscription.getTotalPrice()).toBe(10132);
+  });
 });
