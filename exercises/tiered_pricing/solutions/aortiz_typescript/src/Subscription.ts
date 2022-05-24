fc57c08c86e91e204e8c14601dd686bad713fc6b
Alexander <undergrounder16@gmail.com>
2022-05-24 10:29:30 -0500

    first Test passed

---
+++ exercises/tiered_pricing/solutions/aortiz_typescript/src/Subscription.ts
@@ -0,0 +1,3 @@
+export class Subscription {
+  constructor(private units: number) {}
+}
--- exercises/tiered_pricing/solutions/aortiz_typescript/tests/Subscription.test.ts
+++ exercises/tiered_pricing/solutions/aortiz_typescript/tests/Subscription.test.ts
@@ -1,4 +1,6 @@
-describe("Codelyber", () => {
+import { Subscription } from "../src/Subscription";
+
+describe("Subscription", () => {
   it("can be instantiated without throwing errors", () => {
     expect(() => new Subscription(1)).not.toThrow(TypeError);
   });
