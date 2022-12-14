955adaac9bebbdb2d20e8c7e1d2002c24df8abf8
Adrian Lagartera <adrianvelada9999@gmail.com>
2022-12-14 13:40:17 +0100

    [TIERED_PRICING] 🟢 Added condition to solve

--- exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
@@ -2,11 +2,13 @@ package com.adrianlh.service;
 
 public class TierPriceCalculatorService {
 
-  public static final Integer TIER_TWO_MAX_UNITS = 10;
   private static final Integer TIER_ONE_MAX_UNITS = 2;
+  public static final Integer TIER_TWO_MAX_UNITS = 10;
+  public static final Integer TIER_THREE_MAX_UNITS = 25;
   private static final Integer TIER_ONE_PRICE = 299;
   private static final Integer TIER_TWO_PRICE = 239;
   public static final Integer TIER_THREE_PRICE = 219;
+  public static final Integer TIER_FOUR_PRICE = 199;
 
 
   public Integer calculateTieredPrice(final Integer numberOfLicenses) {
@@ -16,9 +18,13 @@ public class TierPriceCalculatorService {
     } else if (numberOfLicenses <= 10) {
       totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (numberOfLicenses
           - TIER_ONE_MAX_UNITS);
-    } else {
+    } else if (numberOfLicenses <= 25) {
       totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (TIER_TWO_MAX_UNITS
           - TIER_ONE_MAX_UNITS) + TIER_THREE_PRICE * (numberOfLicenses - TIER_TWO_MAX_UNITS);
+    } else {
+      totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (TIER_TWO_MAX_UNITS
+          - TIER_ONE_MAX_UNITS) + TIER_THREE_PRICE * (TIER_THREE_MAX_UNITS - TIER_TWO_MAX_UNITS)
+          + TIER_FOUR_PRICE * (numberOfLicenses - TIER_THREE_MAX_UNITS);
     }
     return totalPrice;
   }
