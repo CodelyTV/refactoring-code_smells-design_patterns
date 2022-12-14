38f8f717ad341510b14713407dc3dcd77135013e
Adrian Lagartera <adrianvelada9999@gmail.com>
2022-12-14 12:11:50 +0100

    [TIERED_PRICING]  🟢 Create Base Test And Service Class

---
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
@@ -0,0 +1,5 @@
+package com.adrianlh.service;
+
+public class TierPriceCalculatorService {
+
+}
---
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/test/java/com/adrianlh/service/TierPriceCalculatorServiceTest.java
@@ -0,0 +1,11 @@
+package com.adrianlh.service;
+
+import org.junit.jupiter.api.Test;
+
+class TierPriceCalculatorServiceTest {
+
+  @Test
+  void calculatePriceOneLicense() {
+    final TierPriceCalculatorService service = new TierPriceCalculatorService();
+  }
+}
