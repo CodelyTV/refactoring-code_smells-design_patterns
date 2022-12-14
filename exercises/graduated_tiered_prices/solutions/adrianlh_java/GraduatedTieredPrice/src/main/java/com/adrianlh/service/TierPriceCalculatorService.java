ab18caf1ea874c08696b335d96b66f09e6463046
Adrian Lagartera <adrianvelada9999@gmail.com>
2022-12-14 12:16:56 +0100

    [TIERED_PRICING] 🔴 Added the method to calculate the price

--- exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
@@ -2,4 +2,7 @@ package com.adrianlh.service;
 
 public class TierPriceCalculatorService {
 
+  public Integer calculateTieredPrice(final Integer numberOfLicenses) {
+    return null;
+  }
 }
--- exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/test/java/com/adrianlh/service/TierPriceCalculatorServiceTest.java
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/test/java/com/adrianlh/service/TierPriceCalculatorServiceTest.java
@@ -1,5 +1,6 @@
 package com.adrianlh.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TierPriceCalculatorServiceTest {
@@ -7,5 +8,9 @@ class TierPriceCalculatorServiceTest {
   @Test
   void calculatePriceOneLicense() {
     final TierPriceCalculatorService service = new TierPriceCalculatorService();
+    final Integer numberOfLicenses = 1;
+    final Integer total = service.calculateTieredPrice(numberOfLicenses);
+    Assertions.assertThat(total).isEqualTo(299);
+
   }
 }
