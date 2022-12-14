067197ba7492d33001841f00c15d8bc5bd86e475
Adrian Lagartera <adrianvelada9999@gmail.com>
2022-12-14 13:10:36 +0100

    [TIERED_PRICING] 🔵 Refactor Price Tiers to Constants

--- exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
@@ -2,9 +2,12 @@ package com.adrianlh.service;
 
 public class TierPriceCalculatorService {
 
-  private final Integer TIER_ONE_MAX_UNITS = 2;
-  private final Integer TIER_ONE_PRICE = 299;
-  private final Integer TIER_TWO_PRICE = 239;
+  public static final Integer TIER_TWO_MAX_UNITS = 10;
+  private static final Integer TIER_ONE_MAX_UNITS = 2;
+  private static final Integer TIER_ONE_PRICE = 299;
+  private static final Integer TIER_TWO_PRICE = 239;
+  public static final Integer TIER_THREE_PRICE = 219;
+
 
   public Integer calculateTieredPrice(final Integer numberOfLicenses) {
     final Integer totalPrice;
@@ -14,8 +17,8 @@ public class TierPriceCalculatorService {
       totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (numberOfLicenses
           - TIER_ONE_MAX_UNITS);
     } else {
-      totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (numberOfLicenses
-          - TIER_ONE_MAX_UNITS - 1) + 219 * (numberOfLicenses - 10);
+      totalPrice = TIER_ONE_PRICE * TIER_ONE_MAX_UNITS + TIER_TWO_PRICE * (TIER_TWO_MAX_UNITS
+          - TIER_ONE_MAX_UNITS) + TIER_THREE_PRICE * (numberOfLicenses - TIER_TWO_MAX_UNITS);
     }
     return totalPrice;
   }
