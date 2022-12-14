89f949dfc80d0f7ce2a5056d3b86686ccf04b22f
Adrian Lagartera <adrianvelada9999@gmail.com>
2022-12-14 12:18:03 +0100

    [TIERED_PRICING] 🟢 (Fake It) Added the literal solution as response

--- exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
+++ exercises/graduated_tiered_prices/solutions/adrianlh_java/GraduatedTieredPrice/src/main/java/com/adrianlh/service/TierPriceCalculatorService.java
@@ -3,6 +3,6 @@ package com.adrianlh.service;
 public class TierPriceCalculatorService {
 
   public Integer calculateTieredPrice(final Integer numberOfLicenses) {
-    return null;
+    return 299;
   }
 }
