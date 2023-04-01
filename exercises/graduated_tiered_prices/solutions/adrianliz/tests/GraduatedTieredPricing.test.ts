8dc3f56de2320f589832e297ee707d10260c092c
Adrián Lizaga <lizagaadrian@gmail.com>
2023-04-01 15:32:11 +0200

    feat(tiered_pricing): 🔵 add parametrised tests

--- exercises/graduated_tiered_prices/solutions/adrianliz/tests/GraduatedTieredPricing.test.ts
+++ exercises/graduated_tiered_prices/solutions/adrianliz/tests/GraduatedTieredPricing.test.ts
@@ -1,3 +1,5 @@
+import each from "jest-each";
+
 import GraduatedTieredPricing from "../src/GraduatedTieredPricing";
 import NumberOfSubscriptionsNotAllowed from "../src/NumberOfSubscriptionsNotAllowed";
 
@@ -7,38 +9,19 @@ describe("Graduated tiered pricing should", () => {
     expect(() => pricing.priceFor(0)).toThrow(NumberOfSubscriptionsNotAllowed);
   });
 
-  it("calculate the price for 1 subscription", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(1)).toBe(299);
-  });
-
-  it("calculate the price for 2 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(2)).toBe(598);
-  });
-
-  it("calculate the price for 3 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(3)).toBe(837);
-  });
-
-  it("calculate the price for 11 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(11)).toBe(2729);
-  });
-
-  it("calculate the price for 26 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(26)).toBe(5994);
-  });
-
-  it("calculate the price for 50 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(50)).toBe(10770);
-  });
-
-  it("calculate the price for 100 subscriptions", () => {
-    const pricing = new GraduatedTieredPricing();
-    expect(pricing.priceFor(100)).toBe(18220);
-  });
+  each([
+    [1, 299],
+    [2, 598],
+    [3, 837],
+    [11, 2729],
+    [26, 5994],
+    [50, 10770],
+    [100, 18220],
+  ]).it(
+    "calculate the price for %i subscriptions",
+    (subscriptions: number, expectedPrice: number) => {
+      const pricing = new GraduatedTieredPricing();
+      expect(pricing.priceFor(subscriptions)).toBe(expectedPrice);
+    }
+  );
 });
