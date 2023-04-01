7ac1660685dbe2a45199ec5a6329c5c2b85dbeb2
Adrián Lizaga <lizagaadrian@gmail.com>
2023-04-01 14:02:59 +0200

    feat(tiered_pricing): 🔴 should return price for any subscription

--- exercises/graduated_tiered_prices/solutions/adrianliz/tests/GraduatedTieredPricing.test.ts
+++ exercises/graduated_tiered_prices/solutions/adrianliz/tests/GraduatedTieredPricing.test.ts
@@ -1,6 +1,12 @@
 import GraduatedTieredPricing from "../src/GraduatedTieredPricing";
+import NumberOfSubscriptionsNotAllowed from "../src/NumberOfSubscriptionsNotAllowed";
 
 describe("Graduated tiered pricing should", () => {
+  it("calculate the price for 0 subscriptions", () => {
+    const pricing = new GraduatedTieredPricing();
+    expect(pricing.priceFor(0)).toThrow(NumberOfSubscriptionsNotAllowed);
+  });
+
   it("calculate the price for 1 subscription", () => {
     const pricing = new GraduatedTieredPricing();
     expect(pricing.priceFor(1)).toBe(299);
@@ -14,6 +20,26 @@ describe("Graduated tiered pricing should", () => {
   it("calculate the price for 3 subscriptions", () => {
     const pricing = new GraduatedTieredPricing();
     expect(pricing.priceFor(3)).toBe(837);
+  });
+
+  it("calculate the price for 11 subscriptions", () => {
+    const pricing = new GraduatedTieredPricing();
+    expect(pricing.priceFor(11)).toBe(2990);
+  });
+
+  it("calculate the price for 26 subscriptions", () => {
+    const pricing = new GraduatedTieredPricing();
+    expect(pricing.priceFor(26)).toBe(6294);
+  });
+
+  it("calculate the price for 50 subscriptions", () => {
+    const pricing = new GraduatedTieredPricing();
+    expect(pricing.priceFor(50)).toBe(10950);
+  });
+
+  it("calculate the price for 100 subscriptions", () => {
+    const pricing = new GraduatedTieredPricing();
+    expect(pricing.priceFor(100)).toBe(21900);
   });
 });
 
